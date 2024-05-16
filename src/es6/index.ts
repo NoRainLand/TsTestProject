8a9ecfa074f66222866fb21354f81501a1d12be3
NoRain <wuxinrufeng@qq.com>
2024-05-16 17:07:23 +0800

    提交

--- src/es6/index.ts
+++ src/es6/index.ts
@@ -1,12 +1,27 @@
+import ClipboardJS from "clipboard";
+import QRCode from "qrcode";
+import VConsole from 'vconsole';
+import "../html/css/style.scss";
 import { myView } from "./ObjectTest.js";
-
-
 export default class index {
-    constructor(){
+    constructor() {
         // new LoadJson();
         new myView();
         // TestHuffman.test();
         // new LoadSk();
+        new ClipboardJS('.btn');
+        QRCode.toDataURL('I am a pony!')
+            .then(url => {
+                console.log(url)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+
+        const vConsole = new VConsole();
+
+        // 接下来即可照常使用 `console` 等方法
+        console.log('Hello world');
     }
 }
 new index();
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require("path");
+var nodeExternals = require("webpack-node-externals");
+
+module.exports = [
+	{
+		entry: "./dist/es6/index.js",
+		output: {
+			path: path.resolve(__dirname, "dist/client"),
+			filename: "index.js",
+		},
+		mode: "production",
+
+		module: {
+			rules: [
+				{
+					test: /\.s[ac]ss$/i,
+					use: [
+						// 将 JS 字符串生成为 style 节点
+						"style-loader",
+						// 将 CSS 转化成 CommonJS 模块
+						"css-loader",
+						// 将 Sass 编译成 CSS
+						"sass-loader",
+					],
+				},
+			],
+		},
+	},
+	// {
+	// 	entry: "./dist/server_temp/server/index.js",
+	// 	output: {
+	// 		path: path.resolve(__dirname, "dist/server"),
+	// 		filename: "index.js",
+	// 	},
+	// 	externals: [nodeExternals()], // 在 externals 选项中使用 webpack-node-externals
+	// 	mode: "production",
+	// 	target: "node",
+	// },
+];
